6941a3e2d44594ec5424b00ae1db0a91b3739b2f
zedin27 <zeidtisnes@gmail.com>
2019-01-04 12:10:32 -0700

    knight dialer problem with different approaches

---
+++ python3/knight_dialer_3.py
@@ -0,0 +1,32 @@
+"""Top-down dynamic programming solution."""
+
+from neighbors import neighbors
+
+def count_sequences(start_position, num_hops):
+	"""Memoization problems requires table to find pre-computed recursive calls.
+
+	When the computation is done, add it to the table.
+	"""
+	table = {}
+
+	def helper(position, num_hops):
+		if (position, num_hops) in table:
+			return table[(position, num_hops)]
+
+		if num_hops is 0:
+			return 1
+
+		else:
+			num_sequences = 0
+			for neighbor in neighbors(position):
+				num_sequences += helper(neighbor, num_hops - 1)
+			table[(position, num_hops)] = num_sequences
+			return num_sequences
+
+	res = helper(start_position, num_hops)
+	return res
+
+if __name__ == '__main__':
+	import args
+	a = args.parse_arguments()
+	print(count_sequences(a.start_position, a.num_hops))
---
+++ python3/knight_dialer_4.py
@@ -0,0 +1,27 @@
+"""Bottom-up approach dynamic programming solution."""
+from neighbors import neighbors
+
+def count_sequences(start_pos, num_hops):
+	"""Tabulation loop by having predetermined tables and iterate each case.
+
+	Same approach of memoization, just with loops this timeself.
+	"""
+	current_case = [0] * 10
+	prior_case = [1] * 10
+	current_num_hops = 1
+
+	while current_num_hops <= num_hops:
+		current_case = [0] * 10
+		current_num_hops += 1
+
+		for pos in range(0, 10):
+			for neighbor in neighbors(pos):
+				current_case[pos] += prior_case[neighbor]
+		prior_case = current_case
+
+	return current_case[start_pos]
+
+if __name__ == '__main__':
+	import args
+	a = args.parse_arguments()
+	print(count_sequences(a.start_position, a.num_hops))
