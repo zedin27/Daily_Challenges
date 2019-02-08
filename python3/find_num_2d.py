60b99a90138b2a2a5ad6dd072f350331720dcbce
zedin27 <ztisneshamzeh@csumb.edu>
2019-02-07 18:06:07 -0800

    fixed the issue where 2D wasn't sorted as it was

--- python3/find_num_2d.py
+++ python3/find_num_2d.py
@@ -1,5 +1,6 @@
 """Search element in a sorted matrix"""
 import random
+import itertools
 
 n = 5
 m = 5
@@ -21,12 +22,15 @@ def findnum_arr(array, num):
     return False
 
 if __name__ == '__main__':
-    multi_array = [[random.randint(0, 20) for x in range(n)] for y in range(m)]
     x = 7
-
-    print(multi_array)
-    print (" ")
-    if not findnum_arr(multi_array, x):
+    multi_array = [[random.randint(0, 200) for x in range(n)] for y in range(m)]
+    # one_array = sorted(list(itertools.chain.from_iterable(multi_array))) Another way
+    one_array = sorted([x for row in multi_array for x in row])
+    sorted_2d = [one_array[i:i+m] for i in range(0, len(one_array), n)]
+    
+    print("multi_array list is: \n{0}\n".format(multi_array))
+    print("sorted 2D array: \n{0}\n".format(sorted_2d))
+    if not findnum_arr(sorted_2d, x):
         print("Not Found")
     else:
         print("Found")
